9042714fd32103e6e5d10694ab48893fdc3a7d58
Jhon <jhon-43769@outlook.com>
2024-04-21 16:55:27 -0400

    Añadir estructura básica para carga diferida

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
-const routes: Routes = [];
+// /auth/
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
