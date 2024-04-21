9eaf5d735fc295caab32ac6220c52dc8179d456f
Jhon <jhon-43769@outlook.com>
2024-04-21 18:10:04 -0400

    Agregar estilos globales y ajustar diseño en layout-page.component.html

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,6 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
     RegisterPageComponent,
     LayoutPageComponent,
   ],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MaterialModule],
 })
 export class AuthModule {}
